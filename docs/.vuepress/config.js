9bc30d2f81f5398d4b70b3862ca2c8b0c75f53f5
darr4i <dosgirrak999@gmail.com>
2023-12-24 22:01:37 +0200

    update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,7 @@ module.exports = {
     }]
   ],
   port: 3030,
-  base: '/edu_db_labs_group1/',
+  base: '/coursework_db/',
   theme: 'cool',
   // dest: 'dist',
   head: [
@@ -76,7 +76,7 @@ module.exports = {
 
     lastUpdated: 'Останнє оновлення', // string | boolean
       // Assumes GitHub. Can also be a full GitLab url.
-    repo: 'https://github.com/AntonenkoTymofii/edu_db_labs_group1',
+    repo: 'https://github.com/darr4i/coursework_db',
     // Customising the header label
     // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: 'Github',
